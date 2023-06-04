f8cca88bc1003c0d3b1fe9aedc346b74aa468462
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2023-06-03 22:41:08 -0400

    Added more door functionality

--- JAFBO Year 4/Assets/Scripts/sceneCharger.cs
+++ JAFBO Year 4/Assets/Scripts/sceneCharger.cs
@@ -11,14 +11,47 @@ public class sceneCharger : MonoBehaviour
     */
     
     public int houseNumber;
+    private bool manualCheck = false;
+
+    //Differe door types available automatic tps you manual needs and additional E press
+    public enum DoorType
+    {
+        Automatic,
+        Manual
+    }
+    public DoorType doorType;
+
+
+
 
     void Update()
     {
-
+        //When you have the right to change scenes (you are standing in an manual door triiger) you can press E to enter it n
+        if(manualCheck == true){
+            if (Input.GetKeyDown(KeyCode.E)){
+                Debug.Log("On your way to scene " + houseNumber);
+                SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+            }
+        }
     }
 
+
     private void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("On your way to scene " + houseNumber);
-        SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+        //When collision if it is automatic you instantly tp
+        if( doorType == DoorType.Automatic){
+            Debug.Log("On your way to scene " + houseNumber);
+            SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+        }
+        //When collision if it is manual you have the right to change scenes with a button
+        if( doorType == DoorType.Manual){
+            manualCheck = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        //Removes right to change scenes with a button press for automation
+        if( doorType == DoorType.Manual){
+            manualCheck = false;
+        }
     }
 }
