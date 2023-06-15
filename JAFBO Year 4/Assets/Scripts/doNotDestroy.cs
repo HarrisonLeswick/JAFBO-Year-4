a96dcc38a5eced8ea5fcbbd93ba427f00de86283
HarrisonLeswick <72412866+HarrisonLeswick@users.noreply.github.com>
2023-06-15 11:18:50 -0400

    Door hub part 1
    Do not destroy made
    Door number fed into it from door
    Door script slightly revamped cause had to much repeated code

---
+++ JAFBO Year 4/Assets/Scripts/doNotDestroy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doNotDestroy : MonoBehaviour
+{
+    public int door;
+    public static doNotDestroy Instance = null;
+
+    private void Awake() { 
+    // If there is an instance, and it's not me, delete myself.
+    
+    if (Instance != null && Instance != this) 
+    { 
+        Destroy(this); 
+    } 
+    else 
+    { 
+        Instance = this; 
+    } 
+    }
+   // Game object with the script will exist even if the scene is changed
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- JAFBO Year 4/Assets/Scripts/sceneCharger.cs
+++ JAFBO Year 4/Assets/Scripts/sceneCharger.cs
@@ -14,6 +14,9 @@ public class sceneCharger : MonoBehaviour, IPointerDownHandler
     */
     
     public int houseNumber;
+    public int doorNumber;
+    public GameObject noDestroy;
+
     private bool manualCheck = false;
 
     //Differe door types available automatic tps you manual needs and additional E press
@@ -25,16 +28,16 @@ public class sceneCharger : MonoBehaviour, IPointerDownHandler
     }
     public DoorType doorType;
 
-
-
+    void Awake() {
+        noDestroy = GameObject.Find("/DoNotDestroy");
+    }
 
     void Update()
     {
         //When you have the right to change scenes (you are standing in an manual door triiger) you can press E to enter it n
         if(manualCheck == true){
             if (Input.GetKeyDown(KeyCode.E)){
-                Debug.Log("On your way to scene " + houseNumber);
-                SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+                useDoor();
             }
         }
     }
@@ -43,8 +46,7 @@ public class sceneCharger : MonoBehaviour, IPointerDownHandler
     private void OnTriggerEnter2D(Collider2D other){
         //When collision if it is automatic you instantly tp
         if( doorType == DoorType.Automatic){
-            Debug.Log("On your way to scene " + houseNumber);
-            SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+            useDoor();
         }
         //When collision if it is manual you have the right to change scenes with a button
         if( doorType == DoorType.Manual){
@@ -63,8 +65,16 @@ public class sceneCharger : MonoBehaviour, IPointerDownHandler
     //Uses unity events to check if the object has been clicked on and if it has teleport you
     // **IMPORTANT FOR USE** Any scene using a Onclick door must have a physics 2d raycaster on its camera and an event system in the hierarchy
          if( doorType == DoorType.OnClick){
-            Debug.Log("On your way to scene " + houseNumber);
-            SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+            useDoor();
         }
     }
+
+//Sends you to your new scene of (housenumber) and to the specific door of (door number)
+    public void useDoor(){
+            Debug.Log("On your way to scene " + houseNumber);
+            noDestroy.GetComponent<doNotDestroy>().door = doorNumber;
+            SceneManager.LoadScene (sceneBuildIndex: houseNumber);
+    }
 }
+
+
